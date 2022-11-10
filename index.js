087620c768afb396e1df0a8256dd6a0da4b2f1b1
sakir101 <sakirhossainf99@gmail.com>
2022-11-10 21:41:30 +0600

    change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000;
-var jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
